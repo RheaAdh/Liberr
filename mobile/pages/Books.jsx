2344a958157aa12958ece7b59977bbc62f84b5fe
Parthiv <pmenon-ctr@wikimedia.org>
2022-11-05 18:46:13 +0530

    added higher order component for design

--- mobile/pages/Books.jsx
+++ mobile/pages/Books.jsx
@@ -2,33 +2,37 @@ import styled from 'styled-components/native';
 import { SafeAreaView } from "react-native-safe-area-context";
 import Loading from '../components/Loading';
 import BookTile from '../components/BookTile';
+import WithGradientPage from './WithGradientPage';
 
 export default function Books() {
   return (
-    <Styled.container>
-      {/* <Loading /> */}
-		<Styled.list 
-			data={[
-				{
-					id: '1',
-					name: 'Michael Scott',
-			  	},
-				// {
-				// 	id: '2',
-				// 	name: 'Michael Scott',
-			  	// }
-			]}
-				keyExtractor={(item) => item.id}
-				renderItem={({item}) => <BookTile  />
-			}	
-		/>
-    </Styled.container>
+	<WithGradientPage>
+		<Styled.container>
+		{/* <Loading /> */}
+			<Styled.list 
+				data={[
+					{
+						id: '1',
+						name: 'Michael Scott',
+					},
+					// {
+					// 	id: '2',
+					// 	name: 'Michael Scott',
+					// }
+				]}
+					keyExtractor={(item) => item.id}
+					renderItem={({item}) => <BookTile  />
+				}	
+			/>
+		</Styled.container>
+	</WithGradientPage>
   );
 }
 
 const Styled = {
   container: styled(SafeAreaView)`
     flex: 1;
+	padding-top: 100px;
   `,
   list: styled.FlatList`
 	padding: 10px
