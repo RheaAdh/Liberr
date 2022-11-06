604cdaeadecec5a8e3b3def2b8c3d92ef337f038
divyashree <divyamanipal01@gmail.com>
2022-11-06 05:55:37 +0530

    image link

--- api/src/models/Copy.js
+++ api/src/models/Copy.js
@@ -17,7 +17,7 @@ const copySchema = new mongoose.Schema(
         },
         imageLink: {
             type: String,
-            default: '',
+            default: 'https://media.istockphoto.com/vectors/stack-of-books-and-open-book-with-red-book-cover-and-white-question-vector-id1256683989?k=20&m=1256683989&s=612x612&w=0&h=7GMvXOULIXJU50TzNKt8ub2sP5s-kpl3RTJh0lRR3Tc=',
         },
         presentOwner: {
             type: mongoose.Schema.Types.ObjectId,
